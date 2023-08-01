910d6082d11bae5f1487c1ade9a810c725f9c637
steven278 <darkavenger347@gmail.com>
2023-08-01 16:02:10 +0700

    project initialization and navbar template with dark mode

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import { MyNavbar } from './components/navbar/navbar'
+
+function App() {
+
+  return (
+    <>
+    <MyNavbar/>
+    </>
+  )
+}
+
+export default App
